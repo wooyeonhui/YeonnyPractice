426b36cd5dd834d4649c65c4b8b1321b9ebaa1ca
Your Name <you@example.com>
2016-12-22 17:47:14 +0900

    lastDance

--- TEST/jQuery시험문제/js/script.js
+++ TEST/jQuery시험문제/js/script.js
@@ -1,10 +1,9 @@
-var 오버 = function(){$(this).css({"background-color":"maroon","color":"white"})}
-var 아웃 =  function(){$(this).css('background-color','initial')}
+var 오버 = function(){$(this).css("background-color","maroon").find('a').css("color","white")}
+var 아웃 =  function(){$(this).css('background-color','initial').find('a').css("color","initial")}
 
+//문서 시작
 
-
-
-
+//hover menu
 $(function(){
   $('nav ul li').mouseover(오버).mouseout(아웃)
   //   $('header ul li').css('background-color','maroon').find('a').css('color','white')
@@ -13,53 +12,112 @@ $(function(){
   //                         function(){
   // $('header ul li').css('background-color','#ccc').find('a').css('color','#000')
  //hover와 enter out에 대해 세심한 관찰을 하자
+
+ //scrollmove
 var a = []
-$('section > div h2').each(function(){
+$('.content section').each(function(){
   a.push($(this).offset().top)
 })
-console.log(a)
+// console.log(a)
 $('nav ul li').click(function(){
   var idx = $(this).index();
-  console.log(idx)
+  // console.log(idx)
   $('body,html').animate({
     scrollTop:a[idx]
   })
 })
-// var slide = {
-//   b : 0,
-//   motion : function(x,y,z,g,d){
-//     g.find('.d-type ul li').eq(z).find('img')css({
-//       'left':x,
-//       'display':d
-//     }).animate({
-//       'left':y
-//     })
-//   },
-//   event:function(){
-//     $('.d-type').on('click','.next',function(){
-//       slide.motion('0','-100%',slide.b,$('.d-type'),'block');
-//       slide.b++
-//       slide.motion('100%','0',slide.b,$('.d-type'),'block');
-//     })
-//   }
-// }
-var idxS;
-  $('.next').click(function(){
-    idxS = $('.d-type ul li').index();
-    $('.d-type ul li').eq(idxS).find('img').animate({
-      left:'-100%'
-    }).parent().next().find('img').css({
-      'display':'block','left':'100%'
-    }).stop().animate({
-      left:0
-    })
+//슬라이드 배너
+
+$('.slidebanner li .next').click(function(){
+  var Swidth = $('.slidebanner').width();
+  console.log(Swidth)
+  $(this).find('img').animate({
+    'left':-Swidth
+  },1000).parent().next().css({
+    'display':'block', 'left': Swidth
+  }).stop().animate({
+    'left' : 0
+  },1000)
+ })
+
+//페이드 배너
+// var Fidx;
+// $('.fadebanner li a').click(function(){
+//   Fidx = $(this).parent().index();
+//   $('.fadebanner li.on').removeClass('on').find('img').fadeOut();
+//   console.log('aaa')
+//   $('.fadebanner li').eq(Fidx).addClass('on').find('img').fadeIn();
+//
+// })
+// $('.fadebanner li.on').find(img).fadeOut(4800,function(){
+//     $(this).removeClass().next().addClass('on')
+// })
+
+//iframe
+// var Ucode =[];
+// Ucode.push($('.movie-view li a').attr("href"))
+// alert(Ucode)
+var Iidx = 0;
+$('.movie-view li').click(function(){
+  Iidx = $(this).index();//Iindx : li의 순서
+  //a link 이동 안하기!!!
+  var Isrc = $('.view iframe').attr('src')//iframe 주소
+  alert(Isrc)
+  var code = $('.movie-view li a').eq(Iidx).attr('href')//li의 영상 코드
+  alert(code)//li 코드 나왔쪄!
+  $('.view iframe').attr('src','https://www.youtube.com/embed/'+code+'?rel=0&amp;controls=0&amp;showinfo=0')
+  console.log($('.view iframe').attr('src','https://www.youtube.com/embed/'+code+'?rel=0&amp;controls=0&amp;showinfo=0'))
+
+
+return false
+
+
+
+  // Iidx = Ucode.index();
+  // console.log(Iidx)
+  // $('.view').find('a').attr(src,"https://www.youtube.com/embed/"+Iidx+"?rel=0&amp;controls=0&amp;showinfo=0")
+
+
+})
+
+
+
+
+//윙배너
+
+$(window).resize(function(){
+  $('.wing').css('left',$('.wing').offset().right + $('.wing').width() - 90)
+});
+$(window).scroll(function(){
+  $('.wing').stop().animate({
+    top : $(this).scrollTop()
+  },1000)
+})
+$(window).trigger('resize')
+
+//pop up
+var Bidx = 0;
+$('.btngroup button').click(function(){
+var Bidx =$('.btngroup button').index();
+$('.btngroup div').eq(Bidx).fadeIn();
+});
 
 
 
 
 
 
-  })
 
 
-  })
+
+
+
+
+
+
+
+
+
+
+
+})
